a7119c5b5699e0b08f5ee22094620aefcf18f88e
nayakazna <nayaka.zna@gmail.com>
2025-11-27 12:21:53 +0700

    refactor: change IOptimizationEngine to use Protocol instead of ABC

--- IOptimizationEngine.py
+++ IOptimizationEngine.py
@@ -1,23 +1,20 @@
 from __future__ import annotations
-from abc import ABC, abstractmethod
+from typing import Protocol, runtime_checkable
 from .IParsedQuery import IParsedQuery
 __all__ = ["IOptimizationEngine"]
 
 
-class IOptimizationEngine(ABC):
-    @abstractmethod
+@runtime_checkable
+class IOptimizationEngine(Protocol):
     def parse_query(self, query: str) -> IParsedQuery:
         ...
 
-    @abstractmethod
     def optimize_query(self, parsed_query: IParsedQuery) -> IParsedQuery:
         ...
 
-    @abstractmethod
     def optimize_query_non_join(self, parsed_query: IParsedQuery) -> IParsedQuery:
         ...
 
-    @abstractmethod
     def get_cost(self, parsed_query: IParsedQuery) -> int:
         ...
 
